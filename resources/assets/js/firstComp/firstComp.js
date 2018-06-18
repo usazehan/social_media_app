46dc3e3e5c1d609527572e540fb1fe8a2edc5c75
Zehan Sunesara <usazehan@gmail.com>
2018-06-18 00:34:49 -0500

    First Commit

---
+++ resources/assets/js/firstComp/firstComp.js
@@ -0,0 +1,23 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class Layout extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+  }
+  clickedBtn = () => {
+    console.log('swag')
+  }
+  render () {
+    return (<div className='home'>
+       This is my first react component
+      </div>)
+  }
+}
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<Layout />, app)
